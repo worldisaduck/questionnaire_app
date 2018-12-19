8f4331722c0426a269e1551423cff5f1a65575aa
Pavel Belyaev <worldisaduck@gmail.com>
2018-12-20 00:20:45 +0300

    Various changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,15 +1,15 @@
 var questions = [
-  'Very very very very very long uestion number two?',
-  'Very very very very very long question number three?',
-  'Very very very very very long question number four?',
-  'Very very very very very long question number five?'
+  'Who is the current Mayor of London (and a University of Law graduate)?',
+  'Take the number 30, divide it by 1/2, and then add 10. What do you get?',
+  'How many Y\'s are there in this picture?',
+  'Magistrates are legally able to use \'reasonable force\' to turn back an alien invasion of the UK as long as the aliens:',
 ];
 
 var possibleAnswers = [
-  ['Answer 1 One', 'Answer 1 Two', 'Answer 1 Three', 'Answer 1 Four'],
-  ['Answer 2 One', 'Answer 2 Two', 'Answer 2 Three', 'Answer 2 Four'],
-  ['Answer 3 One', 'Answer 3 Two', 'Answer 3 Three', 'Answer 3 Four'],
-  ['Answer 3 One', 'Answer 3 Two', 'Answer 3 Three', 'Answer 3 Four']
+  ['Boris Jhonson', 'Sadiq Khan', 'Zac Goldsmith', 'Alan Sugar'],
+  ['15', '25', '40', '70'],
+  ['3', '4', '5', '6'],
+  ['Don\'t have a licence to invade', 'Have\'t received a royal summons', 'Are radioactive', 'Refuse to comply with the Highway Code'],
 ];
 var usersAnswers = []
 
@@ -30,9 +30,23 @@ window.onload = function() {
   var startBtn = document.getElementById('start-btn');
 
   var timerNode = document.getElementById("timer");
-  var timerActive = false;
 
-  startBtn.addEventListener('click', startQuestionnaire);
+  startBtn.addEventListener('click', proceed);
+
+  function proceed(event) {
+    event.preventDefault();
+    var page = this.getAttribute('data-page');
+    if (page == 'initial') {
+      console.log();
+      document.querySelector('p.changing-text').innerHTML = `You'll be asked 4 multiple-choice questions, and you have <b>15</b> seconds to answer each.
+        Each option you chose corresponds to a digit on the safe's padlock. Get all 4 answers
+        correct and unlock the safe! Simple.`
+      this.setAttribute('data-page', 'secondary');
+      this.innerText = '(touch screen to start)';
+    } else if (page == 'secondary') {
+      startQuestionnaire();
+    }
+  }
 
   function startTimer() {
     if (timerActive) {
@@ -62,19 +76,23 @@ window.onload = function() {
     }
   }
 
+  function resetTimer() {
+    document.getElementById('timer').innerHTML = '00:16'
+  }
+
   function startQuestionnaire() {
     var initialHTML = `
       <div class="row" id="question">
         <div class="col-1">
           <h1 id="question-title">Q1</h1>
         </div>
-        <div class="col-6" id="question-text">
+        <div class="col-10" id="question-text">
           <h3>Who is the current Mayor of London (and a University of Law graduate)?</h3>
         </div>
       </div>
       <form id="answers-form">
         <div class="row" id="answers">
-          <div class="col-md-5 offset-md-1">
+          <div class="col-md-8 offset-md-1">
             <div class="answer">
               <input type="radio" name="answer" id="answer1" value="1">
                 <label for="answer1">1. <span>Boris Jhonson</span></label>
@@ -110,7 +128,7 @@ window.onload = function() {
 
     var answersForm = document.getElementById('answers-form');
     var answerNodes = document.querySelectorAll('input[name="answer"]');
-  
+
     answersForm.addEventListener('submit', function(event) {
       event.preventDefault();
       recordAnswer(answersForm.elements['answer'].value);
@@ -120,28 +138,38 @@ window.onload = function() {
     startTimer();
 
     function changeQuestion() {
+
       var currentAnswers = possibleAnswers[currentQuestionIndex];
-      if (questions.length == currentQuestionIndex) finishQuestionnaire();
       var questionNumber = currentQuestionIndex + 1;
       questionTitle.innerHTML = 'Q' + questionNumber;
       questionText.innerHTML = questions[currentQuestionIndex];
       document.querySelector('body').style.backgroundImage = 'url("assets/images/background_' + questionNumber + '.jpg")'
+      if (questions.length == currentQuestionIndex) finishQuestionnaire();
 
       for (i = 0; i < 4; i++) {
         var currentAnswer = answerNodes[i];
         currentAnswer.checked = false;
         currentAnswer.labels[0].childNodes[1].innerHTML = currentAnswers[i]
       }
+      resetTimer();
     }
 
     function recordAnswer(answer) {
       usersAnswers.push(answer);
       currentQuestionIndex += 1;
+      console.log(currentQuestionIndex);
     }
 
     function finishQuestionnaire() {
       var h2 = document.createElement('h2');
-      h2.innerHTML = 'Thank you!';
+      h2.innerHTML =
+        '<div style="width: 50%; margin: auto; text-align: center;">' +
+          'Thank you!<br>' +
+          'Your 4-digit code is:<br>' +
+          '<p style=margin: 25px;>' +
+          usersAnswers.join(' ') +
+          '</p>' +
+        '</div>';
       mainContent.innerHTML = '';
       mainContent.appendChild(h2);
       timerNode.style.display = 'none';
